a6f5004312d9ad38b5c8a12f940929b3ae2a7cd4
your name <julywaltz77@hotmail.com>
2019-08-30 20:48:09 +0800

    修改

--- 21天爬虫学习小组/zhilian.py
+++ 21天爬虫学习小组/zhilian.py
@@ -4,7 +4,7 @@
 # @Author: Cheng Yili
 # @Date: 2019-08-28 21:58:49
 # @LastEditors: Cheng Yili
-# @LastEditTime: 2019-08-29 00:20:49
+# @LastEditTime: 2019-08-30 20:47:54
 # @Email: julywaltz77@hotmail.com
 
 import requests
@@ -20,14 +20,13 @@ head_row = [
     '岗位名称', '招聘公司', '公司类型', '公司规模', '工作地点', '岗位类型', '经验要求', '学历要求', '是否全职',
     '薪资待遇', '岗位链接', '岗位描述'
 ]
-with open('./zhilian/zhilian.csv', 'a', encoding='utf8',
-          newline='') as csvfile:
+with open('./zhilian/zhilian.csv', 'a', encoding='gbk', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(head_row)
 url = 'https://fe-api.zhaopin.com/c/i/sou'
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
 headers = {'User-Agent': ua}
-for i in range(1, 13):
+for i in range(1, 12):
     params = {
         'pageSize': 90 * i,
         'cityId': 801,
@@ -47,11 +46,9 @@ for i in range(1, 13):
     }
     res = requests.get(url, headers=headers, params=params)
     results = res.json()['data']['results']
-    print(params['pageSize'])
     rows = []
-    print(rows)
     for r in results:
-        job_name = r['jobName']
+          job_name = r['jobName']
         company = r['company']
         company_name = company['name']
         company_type = company['type']['name']
@@ -59,7 +56,7 @@ for i in range(1, 13):
         city = r['city']['items'][0]['name']
         job_type = r['jobType']['items'][0]['name']
         work_exp = r['workingExp']['name']
-        edu_level = r['eduLevel']
+        edu_level = r['eduLevel']['name']
         emp_type = r['emplType']
         salary = r['salary']
         pos_url = r['positionURL']
@@ -68,24 +65,24 @@ for i in range(1, 13):
             work_exp, edu_level, emp_type, salary, pos_url
         ]
         rows.append(row)
-    print(rows)
-    for row in rows:
+
+    for row in rows:5
         pos_url = row[-1]
         print(pos_url)
-        sleep(5)
         pos_res = requests.get(pos_url, headers=headers)
+        sleep(2)
         pos_html = pos_res.text
         soup = BeautifulSoup(pos_html, 'lxml')
         des_jobs = soup.find_all(attrs={"class": "describtion"})
         for des_job in des_jobs:
             des_text = des_job.text
             row.append(des_text)
-        print(row)
     print('开始写入')
-    with open('./zhilian/zhilian.csv', 'a', encoding='utf8',
-              newline='') as csvfile:
+    with open('./zhilian/zhilian.csv', 'a', newline='',
+              encoding='gb18030') as csvfile:
         writer = csv.writer(csvfile)
         for row in rows:
+            print(row)
             writer.writerow(row)
     print('写入完成')
 print('全部写入完成')
